61777e9a9b557fa29ee358bccd4cb1d48fd0352a
MRaysa <147266032+MRaysa@users.noreply.github.com>
2025-04-13 13:26:33 +0600

    added footer section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import About from "./components/sections/About/About";
 import Contact from "./components/sections/Contact/Contact";
+import Footer from "./components/sections/Footer/Footer";
 import Home from "./components/sections/Home/Home";
 import Projects from "./components/sections/Projects/Projects";
 import Skills from "./components/sections/Skills/Skills";
@@ -15,6 +16,7 @@ function App() {
       <Projects></Projects>
       <Skills></Skills>
       <Contact></Contact>
+      <Footer></Footer>
     </>
   );
 }
